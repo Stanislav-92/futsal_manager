e950bc5efba83dc75b4dcf8b66a29bacafb60773
Stanislav Chepa <stanislawchepa@gmail.com>
2026-03-12 12:36:28 +0100

    feat: complete project setup, routing, layout and error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import RootLayout from '@/layout/RootLayout';
+import DashboardPage from '@/pages/dashboard/DashboardPage';
+import StatisticsPage from '@/pages/statistics/StatisticsPage';
+import ContactsPage from '@/pages/contacts/ContactsPage';
+import ErrorPage from './pages/error/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <DashboardPage /> },
+      { path: 'statistics', element: <StatisticsPage /> },
+      { path: 'contacts', element: <ContactsPage /> },
+    ],
+  },
+]);
 
 function App() {
-
-  return (
-    <div>
-      Futsal Manager
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/statistics/StatisticsPage.tsx
@@ -0,0 +1,5 @@
+import { Typography } from '@mui/material';
+
+export default function StatisticsPage() {
+  return <Typography variant="h5">Stats</Typography>;
+}
